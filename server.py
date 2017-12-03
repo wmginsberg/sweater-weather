c2cb3d1f50e2cb4e16e37d68e590b5e3cf450c84
Wendy Ginsberg <wmginsberg@gmail.com>
2017-12-03 18:09:07 +0000

    parse puzzle and navigate board with keys

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, redirect, render_template
 import requests
+import random
+from requests_oauthlib import OAuth1
+
+auth = OAuth1("e357f053bbcc4ba79184f22474f47a67", "b6408f1819944435bcf54d49ef9058ea")
 
 
 app = Flask(__name__)
@@ -32,6 +36,30 @@ def get_weather_for_zip(zipc):
 	if (int(deg) > 62):
 		sw = False
 	return render_template('weather.html', sw=sw, town=town, weathers=weathers, deg=deg, info=desc)		
+
+@app.route('/cubes/<qt>')
+def get_cubes(qt):
+	qts = qt.split(',');
+	icons_to_send = []
+	for q in qts:
+		url = 'http://api.thenounproject.com/icons/'+q+'?limit_to_public_domain=1&limit=4'
+		r = requests.get(url,verify=False,auth=auth)
+		print r
+		info = r.json()
+		icons = info['icons']
+		target = random.randint(0, 3)
+		i = 0
+		for icon in icons:
+			if (i == target):
+				attr = icon['attribution']
+				img_url = icon['icon_url']
+				icons_to_send.append({'term':q,'attr':attr,'img_url':img_url})
+				break
+			else: 
+				i += 1
+	return render_template('story.html', icons=icons_to_send)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
